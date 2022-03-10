38d9cfb2abdd5ca68837f4d1f4e01ec2f34a1825
Vinicius Rocha <vinicius.workspace@gmail.com>
2022-03-10 10:18:20 -0300

    refactor: add setupUI for TabViewController

--- solutions/devsprint-lorrayne-paraiso-1/FinanceApp/Screens/TabBarViewController.swift
+++ solutions/devsprint-lorrayne-paraiso-1/FinanceApp/Screens/TabBarViewController.swift
@@ -11,22 +11,25 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemBackground
-        
-        let vc1 = UINavigationController(rootViewController: HomeViewController())
-        let vc2 = UINavigationController(rootViewController: TransfersViewController())
-        
-        vc1.tabBarItem.image = UIImage(systemName: "house.fill")
-        vc2.tabBarItem.image = UIImage(systemName: "arrow.up.arrow.down")
-        
-        vc1.title = "Home"
-        vc2.title = "Transfers"
-        
-        tabBar.tintColor = .systemBlue
-        
-        setViewControllers([vc1,vc2], animated: true)
+        self.setupUI()
     }
-    
+}
 
+//MARK: Private extension - used to store private methods
+private extension UITabBarController{
+    func setupUI(){
+        self.view.backgroundColor = .systemBackground
+        self.tabBar.tintColor = .systemBlue
+        
+        let homeVc = UINavigationController(rootViewController: HomeViewController())
+        let transfersVc = UINavigationController(rootViewController: TransfersViewController())
+        
+        homeVc.tabBarItem.image = UIImage(systemName: "house.fill")
+        transfersVc.tabBarItem.image = UIImage(systemName: "arrow.up.arrow.down")
+        
+        homeVc.title = "Home"
+        transfersVc.title = "Transfers"
+        
+        setViewControllers([homeVc,transfersVc], animated: true)
+    }
 }
