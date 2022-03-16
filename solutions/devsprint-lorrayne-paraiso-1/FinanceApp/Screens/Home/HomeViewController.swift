eab3c77d04c05cbe4344bd8284cf05699ab48815
Vinicius Rocha <vinicius.workspace@gmail.com>
2022-03-16 07:02:58 -0300

    fix: change return at fetchHomeData to HomeModel

--- solutions/devsprint-lorrayne-paraiso-1/FinanceApp/Screens/Home/HomeViewController.swift
+++ solutions/devsprint-lorrayne-paraiso-1/FinanceApp/Screens/Home/HomeViewController.swift
@@ -9,24 +9,43 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    private let service = FinanceService()
+    private let service: FinanceService
+    private let thread: DispatchQueue
 
     private let homeView: HomeView = {
 
         let homeView = HomeView()
         return homeView
     }()
+    
+    init(service: FinanceService = FinanceService(), thread: DispatchQueue = .main) {
+        self.service = service
+        self.thread = thread
+           
+        super.init(nibName: nil, bundle: nil)
+    }
 
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+        
+    
     override func viewDidLoad() {
 
         navigationItem.title = "Finance App"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        service.fetchHomeData { homeData in
 
-        service.fetchHomeData { activities in
+            guard let homeData = homeData else {
+                return
+            }
 
-            DispatchQueue.main.async {
+            let configuration = HomeViewConfiguration(homeData: homeData)
 
-                self.homeView.updateView(with: activities)
+            self.thread.async {
+                self.homeView.updateView(with: configuration)
             }
         }
     }
