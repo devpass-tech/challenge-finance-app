bcdad4c4c8890923585beade4e6d191a598a6cf7
Henrique Cora <henriqueaugusto086@gmail.com>
2022-03-21 20:56:39 -0300

    Cria view configuration

---
+++ solutions/devsprint-renato-sarro-2/FinanceApp/Extensions/ViewConfiguration.swift
@@ -0,0 +1,23 @@
+//
+//  ViewConfiguration.swift
+//  FinanceApp
+//
+//  Created by Henrique Augusto on 21/03/22.
+//
+
+import Foundation
+
+protocol ViewConfiguration {
+    func setupViews()
+    func configViews()
+    func buildViews()
+    func setupConstraints()
+}
+
+extension ViewConfiguration {
+    func setupViews() {
+        configViews()
+        buildViews()
+        setupViews()
+    }
+}
