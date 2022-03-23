705e52b0cdc229007bee813804bf55cb4ff0d806
Anderson Oliveira <cora@MacBook-Pro-de-Cora.local>
2022-03-23 18:18:54 -0300

    elementos basicos da view feitos e chamados na controller

--- solutions/devsprint-renato-sarro-2/FinanceApp/Screens/Transfers/TransfersViewController.swift
+++ solutions/devsprint-renato-sarro-2/FinanceApp/Screens/Transfers/TransfersViewController.swift
@@ -9,7 +9,23 @@ import UIKit
 
 class TransfersViewController: UIViewController {
 
+    private let transferView: UIView
+    
+    init(view: UIView) {
+        transferView = view
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func loadView() {
         self.view = TransfersView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
 }
