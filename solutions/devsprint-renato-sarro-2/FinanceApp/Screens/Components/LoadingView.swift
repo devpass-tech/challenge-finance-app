dc975674e20c00a29332b05f89ad65fc0137d797
Cassia <cassia.developer@gmail.com>
2022-03-23 09:25:16 -0300

    [US] Implementar LoadingView

---
+++ solutions/devsprint-renato-sarro-2/FinanceApp/Screens/Components/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  FinanceApp
+//
+//  Created by cassia franco on 23/03/22.
+//
+
+import UIKit
+
+class LoadingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
