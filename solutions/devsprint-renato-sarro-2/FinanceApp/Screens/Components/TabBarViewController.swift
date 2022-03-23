8a8bad4ae58441e01f0a55bdc645ff5a88532798
p-cardozo <contato.pcardozo@gmail.com>
2022-03-23 20:35:03 -0300

    Criando tabBar

---
+++ solutions/devsprint-renato-sarro-2/FinanceApp/Screens/Components/TabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarViewController.swift
+//  FinanceApp
+//
+//  Created by Patricia Cardozo on 23/03/22.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
