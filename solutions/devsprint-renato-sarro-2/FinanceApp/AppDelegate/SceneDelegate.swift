cd4057a911343e810bec97b7be319d41370f68c0
p-cardozo <contato.pcardozo@gmail.com>
2022-03-24 19:24:36 -0300

    arrumando arquivo tabBar

--- solutions/devsprint-renato-sarro-2/FinanceApp/AppDelegate/SceneDelegate.swift
+++ solutions/devsprint-renato-sarro-2/FinanceApp/AppDelegate/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.rootViewController = UINavigationController(rootViewController: ContactListViewController())
+        self.window?.rootViewController = UINavigationController(rootViewController: TabBarViewController())
         self.window?.windowScene = windowScene
         self.window?.makeKeyAndVisible()
     }
--- solutions/devsprint-renato-sarro-2/FinanceApp/Screens/Components/TabBarViewController.swift
+++ solutions/devsprint-renato-sarro-2/FinanceApp/Screens/Components/TabBarViewController.swift
@@ -2,28 +2,53 @@
 //  TabBarViewController.swift
 //  FinanceApp
 //
-//  Created by Patricia Cardozo on 23/03/22.
+//  Created by Patricia Cardozo on 24/03/22.
 //
 
 import UIKit
 
 class TabBarViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTabBar()
+        setupControllers()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupTabBar() {
+        view.backgroundColor = .systemBackground
+        UITabBar.appearance().barTintColor = .systemBackground
+        tabBar.tintColor = .systemBlue
+        
+        self.tabBar.layer.borderWidth = 0.5
+        self.tabBar.layer.borderColor = UIColor.gray.cgColor
     }
-    */
-
+    
+    func setupControllers() {
+        
+        viewControllers = [
+            creatingNavigationController(with: HomeViewController(),
+                                       title: "Home",
+                                       image: UIImage(named: "house.fill")
+                                      ),
+            creatingNavigationController(with: TransfersViewController(),
+                                       title: "Transfers",
+                                       image: UIImage(named: "arrow.up.arrow.down")
+                                      )
+        ]
+    }
+    
+    private func creatingNavigationController(with rootViewController: UIViewController,
+                                            title: String,
+                                            image: UIImage?) -> UIViewController {
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        
+        navigationController.tabBarItem.title = title
+        navigationController.tabBarItem.image = image
+        navigationController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        
+        return navigationController
+        
+    }
 }
