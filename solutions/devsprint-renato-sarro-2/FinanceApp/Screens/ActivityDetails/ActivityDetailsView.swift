136ed4178f9975495e59190314ffcf23595dea50
GilmarMMJr <gilmar_mendonca@live.com>
2022-03-27 13:50:41 -0300

    Implementar ActivityDetailsView
    - [x]  Implementar a interface em um arquivo separado com o nome ActivityDetailsView.swift
    - [ ]  Implementar service pra trazer os detalhes da atividade
    - [ ]  O componente deve mostrar uma imagem, o nome, a categoria, o valor e horário da compra, e um botão com o título “Report a issue”
    - [ ]  A interface deve ser implementada conforme o anexo

--- solutions/devsprint-renato-sarro-2/FinanceApp/Screens/ActivityDetails/ActivityDetailsView.swift
+++ solutions/devsprint-renato-sarro-2/FinanceApp/Screens/ActivityDetails/ActivityDetailsView.swift
@@ -7,6 +7,145 @@
 
 import UIKit
 
-class ActivityDetailsView: UIView {
+class ActivityDetailsView: UIView, ViewConfiguration {
+    
+    lazy var activityImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "bag.circle.fill")
+        image.tintColor = UIColor(red: 175/255, green: 82/255, blue: 222/255, alpha: 1)
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Mall"
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var categoryLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Shopping"
+        label.textColor = .black.withAlphaComponent(0.6)
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "$100.00"
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 34)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var timeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "8:57 AM"
+        label.textColor = .black.withAlphaComponent(0.6)
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var reportButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Report a issue", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 14
+        return button
+    }()
+        
+    lazy var headStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 8
+        return stackView
+    }()
 
+    lazy var bodyStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        self.buildViews()
+        self.configViews()
+        self.setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configViews() {
+        self.addSubview(self.headStackView)
+        self.addSubview(self.bodyStackView)
+        self.headStackView.addArrangedSubview(self.activityImage)
+        self.headStackView.addArrangedSubview(self.nameLabel)
+        self.headStackView.addArrangedSubview(self.categoryLabel)
+        self.bodyStackView.addArrangedSubview(self.priceLabel)
+        self.bodyStackView.addArrangedSubview(self.timeLabel)
+        self.addSubview(self.reportButton)
+            }
+    
+    func buildViews() {
+        self.backgroundColor = .white
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+
+
+            self.headStackView.safeAreaLayoutGuide.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 34),
+            self.headStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            self.headStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+
+            self.activityImage.heightAnchor.constraint(equalToConstant: 100),
+            self.activityImage.widthAnchor.constraint(equalToConstant: 100),
+
+            self.nameLabel.heightAnchor.constraint(equalToConstant: 22),
+            self.categoryLabel.heightAnchor.constraint(equalToConstant: 20),
+
+            self.bodyStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            self.bodyStackView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            self.bodyStackView.leadingAnchor.constraint(equalTo: self.headStackView.leadingAnchor),
+            self.bodyStackView.trailingAnchor.constraint(equalTo: self.headStackView.trailingAnchor),
+
+            self.priceLabel.heightAnchor.constraint(equalToConstant: 41),
+            self.priceLabel.widthAnchor.constraint(equalToConstant: 413),
+
+            self.timeLabel.heightAnchor.constraint(equalToConstant: 20),
+            self.timeLabel.widthAnchor.constraint(equalToConstant: 413),
+
+            self.reportButton.heightAnchor.constraint(equalToConstant: 56),
+            self.reportButton.leadingAnchor.constraint(equalTo: self.headStackView.leadingAnchor),
+            self.reportButton.trailingAnchor.constraint(equalTo: self.headStackView.trailingAnchor),
+            self.reportButton.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -17.5)
+
+        ])
+    }
+    
 }
