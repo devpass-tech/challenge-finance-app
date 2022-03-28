4d230e9b427a4457738a099adf7f5137253b1187
Paola Soares <paolaalcantara1@yahoo.com.br>
2022-03-28 20:18:44 -0300

    adding constraints in confirmationView

--- solutions/devsprint-renato-sarro-2/FinanceApp/Screens/Confirmation/ConfirmationView.swift
+++ solutions/devsprint-renato-sarro-2/FinanceApp/Screens/Confirmation/ConfirmationView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+//import XCTest
 
 class ConfirmationView: UIView {
     
@@ -14,38 +15,84 @@ class ConfirmationView: UIView {
         let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
         image.image = UIImage(named: "checkmark.circle.fill")
-        
-        
+        image.tintColor = UIColor(cgColor: .init(red: 0.190, green: 0.840, blue: 0.297, alpha: 1))
         return image
+    }()
+    
+    lazy var titleConfirmation: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Your transfer was successful"
+        label.font = UIFont.systemFont(ofSize: 35)
+        label.textColor = .black
+        label.textAlignment = .center
+
+        
+        return label
+    }()
+    
+   lazy var niceConfirmation: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Nice", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        button.backgroundColor = UIColor(cgColor: .init(red: 0.190, green: 0.840, blue: 0.297, alpha: 1))
+        button.layer.cornerRadius = 15
+   
+        return button
+    
     }()
     
     init() {
         super.init(frame: .zero)
-        bildViews()
-        configConstraints()
         configViews()
+        biuldViews()
+        configConstraintsImage()
+        configConstraintsTitle()
+        configConstraintsNice()
+        
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     func configViews() {
         backgroundColor = UIColor.white
     }
-    
-    func bildViews() {
+
+    func biuldViews() {
         addSubview(imageConfirmation)
+        addSubview(titleConfirmation)
+        addSubview(niceConfirmation)
     }
-    
-    private func configConstraints(){
-        NSLayoutConstraint.activate([
-            self.imageConfirmation.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            self.imageConfirmation.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            self.imageConfirmation.widthAnchor.constraint(equalToConstant: 100),
-            self.imageConfirmation.heightAnchor.constraint(equalToConstant: 100)
+    func configConstraintsImage() {
+    NSLayoutConstraint.activate([
+        imageConfirmation.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 250),
+        imageConfirmation.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 60),
+        imageConfirmation.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -60),
+        imageConfirmation.heightAnchor.constraint(equalToConstant: 200)
+    ])
+}
+    func configConstraintsTitle() {
+    NSLayoutConstraint.activate([
+
+        titleConfirmation.topAnchor.constraint(equalTo: imageConfirmation.bottomAnchor, constant: 8),
+        titleConfirmation.centerXAnchor.constraint(equalTo: centerXAnchor),
+        titleConfirmation.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+        titleConfirmation.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+        titleConfirmation.heightAnchor.constraint(equalToConstant: 56)
+    ])
+}
+    func configConstraintsNice() {
+    NSLayoutConstraint.activate([
         
-        ])
-    
-    }
+        niceConfirmation.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -34),
+        niceConfirmation.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+        niceConfirmation.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
+        niceConfirmation.heightAnchor.constraint(equalToConstant: 56)
+    ])
+}
+
+     
 }
