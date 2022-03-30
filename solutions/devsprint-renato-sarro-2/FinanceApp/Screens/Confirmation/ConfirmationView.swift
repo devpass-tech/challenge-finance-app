699f03b05f29e29389eae3f3b8618b184119f886
Renato Sarro <cora@MacBook-Pro-de-Cora-5.local>
2022-03-30 20:02:54 -0300

    Ajusta layout.

--- solutions/devsprint-renato-sarro-2/FinanceApp/Screens/Confirmation/ConfirmationView.swift
+++ solutions/devsprint-renato-sarro-2/FinanceApp/Screens/Confirmation/ConfirmationView.swift
@@ -23,24 +23,24 @@ class ConfirmationView: UIView {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Your transfer was successful"
-        label.font = UIFont.systemFont(ofSize: 35)
+        label.font = .systemFont(ofSize: 17, weight: .bold)
         label.textColor = .black
         label.textAlignment = .center
-
+        
         
         return label
     }()
     
-   lazy var niceConfirmation: UIButton = {
+    lazy var niceConfirmation: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Nice", for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
         button.backgroundColor = UIColor(cgColor: .init(red: 0.190, green: 0.840, blue: 0.297, alpha: 1))
         button.layer.cornerRadius = 15
-   
+        
         return button
-    
+        
     }()
     
     init() {
@@ -52,47 +52,47 @@ class ConfirmationView: UIView {
         configConstraintsNice()
         
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     func configViews() {
         backgroundColor = UIColor.white
     }
-
+    
     func biuldViews() {
         addSubview(imageConfirmation)
         addSubview(titleConfirmation)
         addSubview(niceConfirmation)
     }
     func configConstraintsImage() {
-    NSLayoutConstraint.activate([
-        imageConfirmation.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 250),
-        imageConfirmation.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 60),
-        imageConfirmation.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -60),
-        imageConfirmation.heightAnchor.constraint(equalToConstant: 200)
-    ])
-}
+        NSLayoutConstraint.activate([
+            imageConfirmation.centerYAnchor.constraint(equalTo: centerYAnchor),
+            imageConfirmation.centerXAnchor.constraint(equalTo: centerXAnchor),
+            imageConfirmation.widthAnchor.constraint(equalToConstant: 64),
+            imageConfirmation.heightAnchor.constraint(equalToConstant: 64)
+        ])
+    }
     func configConstraintsTitle() {
-    NSLayoutConstraint.activate([
-
-        titleConfirmation.topAnchor.constraint(equalTo: imageConfirmation.bottomAnchor, constant: 8),
-        titleConfirmation.centerXAnchor.constraint(equalTo: centerXAnchor),
-        titleConfirmation.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
-        titleConfirmation.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
-        titleConfirmation.heightAnchor.constraint(equalToConstant: 56)
-    ])
-}
+        NSLayoutConstraint.activate([
+            
+            titleConfirmation.topAnchor.constraint(equalTo: imageConfirmation.bottomAnchor, constant: 8),
+            titleConfirmation.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleConfirmation.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            titleConfirmation.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            titleConfirmation.heightAnchor.constraint(equalToConstant: 56)
+        ])
+    }
     func configConstraintsNice() {
-    NSLayoutConstraint.activate([
-        
-        niceConfirmation.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -34),
-        niceConfirmation.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
-        niceConfirmation.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
-        niceConfirmation.heightAnchor.constraint(equalToConstant: 56)
-    ])
-}
-
-     
+        NSLayoutConstraint.activate([
+            
+            niceConfirmation.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -34),
+            niceConfirmation.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            niceConfirmation.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
+            niceConfirmation.heightAnchor.constraint(equalToConstant: 56)
+        ])
+    }
+    
+    
 }
