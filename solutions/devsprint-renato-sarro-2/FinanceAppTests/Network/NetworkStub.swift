2669fcc06a4d19ac83c7555aef9d0d1a7e4dca82
Henrique Cora <henriqueaugusto086@gmail.com>
2022-03-31 10:50:15 -0300

    imlementa teste de sucesso no network/api client

--- solutions/devsprint-renato-sarro-2/FinanceAppTests/Network/NetworkStub.swift
+++ solutions/devsprint-renato-sarro-2/FinanceAppTests/Network/NetworkStub.swift
@@ -15,13 +15,58 @@ struct NetworkErrorStub: URLSessionProtocol {
     }
 }
 
+struct NetworkSuccessStub: URLSessionProtocol {
+    func fetchData(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        let data = json.data(using: .utf8)!
+        let urlResponse = HTTPURLResponse(url: URL(string: ApiRequestSuccessMock().urlString)!, statusCode: 200, httpVersion: nil, headerFields: nil)
+        completionHandler(data, urlResponse, nil)
+    }
+}
+
 struct ErrorDummy: Error {}
+struct FakeDecodable: Decodable {}
 
-struct URLSessionDataTaskDummy {}
-
-struct ApiRequestMock: ApiRequest {
-    var urlString: String = "https://trinhngocthuyen.github.io/posts/tech/method-dispatch-in-swift/"
+struct ApiRequestErrorMock: ApiRequest {
+    var urlString: String = "https://raw.githubusercontent.com/devpass-tech/challenge-finance-app/main/api/home_endpoint.json"
     var method: ApiMethod = .get
 }
 
-struct FakeDecodable: Decodable {}
+struct ApiRequestSuccessMock: ApiRequest {
+    var urlString: String = "https://raw.githubusercontent.com/devpass-tech/challenge-finance-app/main/api/home_endpoint.json"
+    var method: ApiMethod = .get
+}
+
+let json = """
+{
+    "balance": 15459.27,
+    "savings": 1000.0,
+    "spending": 500.0,
+    "activity": [
+        {
+            "name": "Mall",
+            "price": 100.0,
+            "time": "8:57 AM"
+        },
+        {
+            "name": "Food Court",
+            "price": 100.0,
+            "time": "8:57 AM"
+        },
+        {
+            "name": "Oceanic Airlines",
+            "price": 100.0,
+            "time": "8:57 AM"
+        },
+        {
+            "name": "Gym Membership",
+            "price": 100.0,
+            "time": "8:57 AM"
+        },
+        {
+            "name": "Private Transport",
+            "price": 100.0,
+            "time": "8:57 AM"
+        }
+    ]
+}
+"""
--- solutions/devsprint-renato-sarro-2/FinanceAppTests/Network/NetworkTests.swift
+++ solutions/devsprint-renato-sarro-2/FinanceAppTests/Network/NetworkTests.swift
@@ -12,14 +12,16 @@ import XCTest
 
 class NetworkTests: XCTestCase {
     
-    let networkStub = NetworkErrorStub()
-    lazy var sut = ApiClient(urlSession: networkStub)
+    let networkErrorStub = NetworkErrorStub()
+    let networkSuccessStub = NetworkSuccessStub()
+    lazy var sut: ApiClient? = nil
     
     func test_shouldReturnErrorInApiClient() {
+        sut = ApiClient(urlSession: networkErrorStub)
         var result: Result<FakeDecodable, Error>?
         
         // 1 given
-        sut.fetchData(request: ApiRequestMock()) {
+        sut?.fetchData(request: ApiRequestErrorMock()) {
             result = $0
         }
         
@@ -30,5 +32,25 @@ class NetworkTests: XCTestCase {
         default:
             XCTFail("Error not be nil")
         }
+    }
+    
+    func test_shoulReturnSuccessInApiClient() {
+        sut = ApiClient(urlSession: NetworkSuccessStub())
+        var result: Result<HomeModel, Error>?
+        
+        sut?.fetchData(request: ApiRequestSuccessMock(), completion: {
+            result = $0
+        })
+        
+        switch result {
+        case .success(let homeModel):
+            XCTAssertNotNil(homeModel)
+        default:
+            XCTFail("request cant failure, should return home model")
+        }
+    }
+    
+    override func tearDown() {
+        sut = nil
     }
 }
