e65c524eb120babe734236a39537c85046dea2f4
Rodrigo Borges <rborges.soares@gmail.com>
2022-06-14 18:51:28 -0300

    Base project for Dev Sprint Julio Fernandes #4

---
+++ solutions/devsprint-julio-fernandes-4/FinanceApp/Models/HomeModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeModel.swift
+//  FinanceApp
+//
+//  Created by Rodrigo Borges on 04/02/22.
+//
+
+import Foundation
+
+struct HomeModel: Decodable {
+
+}
---
+++ solutions/devsprint-julio-fernandes-4/FinanceApp/Screens/Home/HomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HomeViewController.swift
+//  FinanceApp
+//
+//  Created by Rodrigo Borges on 30/12/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    private let service = FinanceService()
+
+    private let homeView: HomeView = {
+
+        let homeView = HomeView()
+        return homeView
+    }()
+
+    override func viewDidLoad() {
+
+        navigationItem.title = "Finance App"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        service.fetchHomeData { activities in
+
+            DispatchQueue.main.async {
+
+                self.homeView.updateView(with: activities)
+            }
+        }
+    }
+
+    override func loadView() {
+        self.view = homeView
+    }
+}
