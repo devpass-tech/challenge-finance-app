57ca150e3b56b6106b9d8ebaa2f99def9ddad362
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2022-06-28 22:35:12 -0300

    removido xbproject

---
+++ solutions/devsprint-julio-fernandes-4/FinanceApp/Models/AccountSummaryViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  AccountSummaryViewModel.swift
+//  FinanceApp
+//
+//  Created by Guilherme Golfetto on 26/06/22.
+//
+
+import Foundation
+
+struct AccountSummaryViewModel {
+    let balance: String
+    let savings: String
+    let spending: String
+}
