eeef208d533d15407b76f63ed1b815f8e40941fe
Anita Spieker - Vortigo <anita.spieker.vortigo@superdigital.com.br>
2022-09-01 16:45:54 -0300

    loading screen created

---
+++ solutions/devsprint-julio-fernandes-6/FinanceApp/Models/ViewCodeProtocol.swift
@@ -0,0 +1,30 @@
+//
+//  ViewCodeProtocol.swift
+//  FinanceApp
+//
+//  Created by Joao Gripp on 01/09/22.
+//
+import UIKit
+
+protocol ViewCodeProtocol {
+    func setupView()
+    func buildViewHierarchy()
+    func setupConstraints()
+    func setupAdditionalConfiguration()
+}
+
+extension ViewCodeProtocol {
+    func setupView() {
+        buildViewHierarchy()
+        setupConstraints()
+        setupAdditionalConfiguration()
+    }
+}
+
+extension ViewCodeProtocol where Self: UITableViewCell {
+    func setupAdditionalConfiguration() {
+        backgroundColor = .clear
+        contentView.backgroundColor = .clear
+        selectionStyle = .none
+    }
+}
