10ffa4b1fe45baa7c73ee6dc1ec11dbef2e1da3f
Filipe Marques <filipi.marques@gmail.com>
2022-09-01 09:22:31 -0300

    feat: add model data in struct and call api to get activities information

--- solutions/devsprint-julio-fernandes-6/FinanceApp/Service/FinanceService.swift
+++ solutions/devsprint-julio-fernandes-6/FinanceApp/Service/FinanceService.swift
@@ -8,9 +8,29 @@
 import Foundation
 
 class FinanceService {
-
+    
     func fetchHomeData(completion: @escaping ([String]) -> Void) {
-
-        completion(["Mall", "Food Court", "Oceanic Airlines", "Gym Membership", "Private Transport"])
+        
+        guard let url = FinanceService.homeApiPath else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            
+            do {
+                let homeData = try JSONDecoder().decode(HomeModel.self, from: data)
+                completion(homeData.activity.map({$0.name}))
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+            
+        }.resume()
     }
+}
+
+extension FinanceService {
+    
+    static let homeApiPath = URL(string:"https://raw.githubusercontent.com/devpass-tech/challenge-finance-app/main/api/home_endpoint.json")
+    
+    
 }
